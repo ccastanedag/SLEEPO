3a74d1b66c87506692929dc74c0075b6a355c356
Carlos Castañeda <carlos.castaneda.gutierrez@gmail.com>
2017-01-28 11:23:09 -0500

    Integrate with moment.js
    I have integrate the moment.js library which allow easily to add or substract minutes to a specific time. The back logic of step2a and step2b are ready

--- js/main.js
+++ js/main.js
@@ -25,11 +25,79 @@ $(document).ready(function(){
 
 
   }
-  
-  // Allow to navigate to other steps
   $("button").click(function(){
+    // Allow to navigate to other steps
     nextStep = $(this).data('nextstep');
-    stepForward(counterStep,nextStep);
+
+    //Move forward on those steps which dont require
+    //any special calculation (step0 step1)
+    if(nextStep!=="step3a" && nextStep!=="step3b")
+      stepForward(counterStep,nextStep);
+
+    // For thos steps which require calculations, we must first
+    //validate its datafields to then use stepForward function
+    if(nextStep==="step3a")
+    {
+      // Validate if all data field on Step 2a were set
+      correctFlag = true; // Asume all fields are set correctly
+      allSelect = $("select.select-step2a");
+      for(i=0; i<allSelect.length; i++){
+        if(allSelect[i].value === ""){
+          correctFlag = false;
+        }
+      }
+      if(!correctFlag){
+        alert("ERROR: Please enter the time you want to wake up");
+      }
+      else {
+        //TODO: Make Calculations to render on step 3a
+        hour = $("select[name='hour'].select-step2a option:selected").text();
+        minute= $("select[name='minute'].select-step2a option:selected").text();
+        timelapse = $("select[name='timelapse'].select-step2a option:selected").text();
+        timeString = hour.concat(":",minute," "+timelapse);
+        formatTime = "hh:mm A";
+
+        /*NOTE: "Sleep Cycles Theory" recommends to sleep 5-6 cycles (each one of 90 minutes),
+          less cycles are acceptable but might reduce the efficiency of the method.
+
+         For this reason there 4 alternative to show:
+          - badtime = 3 cycles (works but not recommended)
+          - normalTime = 4 cycles
+          - goodTime = 5 cycles  (works good)
+          - greatTime = 6 cycles (recommended by experts)*/
+
+        badTime = moment(timeString, formatTime).subtract(90*3,'m').format(formatTime);
+        normalTime = moment(timeString, formatTime).subtract(90*4,'m').format(formatTime);
+        goodTime = moment(timeString, formatTime).subtract(90*5,'m').format(formatTime);
+        greatTime = moment(timeString, formatTime).subtract(90*6,'m').format(formatTime);
+
+        alert("Bad: "+badTime+"\n"+"Normal: "+normalTime+"\n"+"Good: "+goodTime+"\n"+"Great: "+greatTime+"\n");
+        stepForward(counterStep,nextStep);
+      }
+    }
+
+    if(nextStep==="step3b")
+    {
+      // Validate if all data field on Step 2b were set
+      correctFlag = true; // asume all fields are set correctly
+      allSelect = $("select.select-step2b");
+      for(i=0; i<allSelect.length; i++){
+        if(allSelect[i].value === ""){
+          correctFlag = false;
+        }
+      }
+      if(!correctFlag){
+        alert("ERROR: Please enter the time you plan to fall asleep");
+      }
+      else {
+        //TODO: Make Calculations to render on step 3b
+        hour = $("select[name='hour'].select-step2b option:selected").text();
+        minute= $("select[name='minute'].select-step2b option:selected").text();
+        timelapse = $("select[name='timelapse'].select-step2b option:selected").text();
+        alert("Los datos capturados son: "+hour+":"+minute+" "+timelapse);
+        stepForward(counterStep,nextStep);
+      }
+    }
   });
 
   // Add the materialize behavior to select items
